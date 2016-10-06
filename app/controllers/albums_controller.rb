ba0c2271a434c798d807922bcffc6b19a096eea7
Joseph McBride <jmcbriddle@gmail.com>
2016-10-06 12:03:08 -0700

    adds album views

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,23 @@
 class AlbumsController < ApplicationController
+  def new
+    @band = Band.find(params[:band_id])
+  end
+
+  def create
+    album = Album.new(album_params)
+    if album.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    @album = Album.includes(:tracks).find(params[:id])
+  end
+  private
+
+  def album_params
+    params.require(:album).permit(:live_album, :title, :band_id)
+  end
 end
